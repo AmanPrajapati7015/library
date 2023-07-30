864a473131019698cbb7b94dbf85d2ca09bc7477
Aman <aman.prajapati7022@gmail.com>
2023-07-30 13:59:45 +0530

    Toggles read status

--- script.js
+++ script.js
@@ -7,11 +7,48 @@ function Book (title, author, pages, isRead){
     this.isRead = isRead;
 }
 
+Book.prototype.toggleRead = function(){
+    this.isRead = !this.isRead
+}
+
+function toggleRead4All(e){
+    let i = +(e.target.parentElement.getAttribute("data-index"))
+    myLibrary[i].toggleRead()
+
+    e.target.textContent = myLibrary[i].isRead ? "Read" : "Not Read";
+    e.target.classList.remove("read")
+    e.target.classList.remove("not-read")
+    e.target.classList.add(myLibrary[i].isRead ? "read" : "not-read")
+}
+
+
+
+
+
 const Book1 = new Book("good vibes good lives", "Vex King", 250, true);
 const Book2 = new Book("power of subconsious mind", "josph morphy", 342, false);
 
 myLibrary.push(Book1)
 myLibrary.push(Book2)
+
+
+
+
+
+// these functions will be called by html elements check html file for their call.
+function showForm(){
+    let form = document.querySelector(".new-book-form")
+    let formBackground = document.querySelector(".form-background")
+    formBackground.classList.add("active")
+    form.classList.add("active")
+}
+function removeForm(){
+    let form = document.querySelector(".new-book-form")
+    let formBackground = document.querySelector(".form-background")
+    formBackground.classList.remove("active")
+    form.classList.remove("active")
+}
+
 
 
 
@@ -50,19 +87,9 @@ function addBookToLibrary(){
 
 
 
-function showForm(){
-    let form = document.querySelector(".new-book-form")
-    let formBackground = document.querySelector(".form-background")
-    formBackground.classList.add("active")
-    form.classList.add("active")
-}
 
-function removeForm(){
-    let form = document.querySelector(".new-book-form")
-    let formBackground = document.querySelector(".form-background")
-    formBackground.classList.remove("active")
-    form.classList.remove("active")
-}
+
+
 
 
 
@@ -73,7 +100,11 @@ function displayBooks(){
     // for a fresh start!!
     booksContainer.innerHTML = "" 
 
-    myLibrary.forEach(book => {
+    let i = 0;
+    let book;
+    while (i < myLibrary.length){
+        book = myLibrary[i]
+
         let bookCard = document.createElement("div");
         bookCard.classList.add("book");
 
@@ -90,17 +121,25 @@ function displayBooks(){
         bookCard.appendChild(pagesPara);
 
         let readStatus = document.createElement("button");
+        readStatus.classList.add("read-status") 
         readStatus.textContent = book.isRead ? "Read" : "Not Read";
         readStatus.classList.add(book.isRead ? "read" : "not-read")
+        readStatus.addEventListener("click", toggleRead4All)    //important!!
         bookCard.appendChild(readStatus);
 
         let remove = document.createElement("button");
         remove.textContent = "Remove";
         bookCard.appendChild(remove);
 
+        bookCard.setAttribute("data-index", i)
+
         booksContainer.appendChild(bookCard)
-        
-    });
+
+
+        i++
+    }
+
+
 }
 
 
